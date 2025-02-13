58e222de9d5fd3403589a8a06ec969362082e9a0
Montythakur9870 <132164798+Montythakur9870@users.noreply.github.com>
2025-02-13 23:26:39 +0530

    git change

--- backend/server.js
+++ backend/server.js
@@ -47,13 +47,11 @@ mongoose.connect(uri)
   });
 
 
-// Serve static assets if in production mode
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'frontend/build')));
+  app.use(express.static(path.join(__dirname, '../frontend/build')));
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, '../frontend', 'build', 'index.html'));
   });
-}
+  
 
 // Import and mount routes
 const userRoutes = require("./routes/userRoute");
